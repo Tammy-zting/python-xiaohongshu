2177ab60fd6b05ef6082e2475946c96b50e48236
314172670 <314172670@qq.com>
2020-08-11 17:56:48 +0800

    fix ssl

--- src/imagetest.py
+++ src/imagetest.py
@@ -1,14 +1,30 @@
 #coding=utf-8
+# import ssl
 import re
-import urllib
-import requests
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen, Request
 
 #获取页面信息
 def getHtml(url):
     #Urllib 模块提供了读取web页面数据的接口，我们可以像读取本地文件一样读取www和ftp上的数据。
-    page=urllib.urlopen(url)
-    html=page.read()
-    return html
+    # response = urllib.request.urlopen(request)
+    # page_html= response.read()
+    # page_soup = urllib.request(page_html, "html.parser")
+    # print (page_soup)
+    # return page_soup
+
+    # ctx = ssl.create_default_context()
+    # ctx.check_hostname = False
+    # ctx.verify_mode = ssl.CERT_NONE
+
+    # request = Request(url)
+    response = urlopen(url)
+    print(response)
+    page_html = response.read()
+    print(page_html)
+    page_soup = soup(page_html, "html.parser")
+    return page_soup
 
 #获取图片并下载
 def getImg(html):
@@ -17,7 +33,7 @@ def getImg(html):
     imgre = re.compile(reg)
     #读取html中符合我们正则表达式的数据
     imglist = re.findall(imgre,html)
-    print imglist
+    print (imglist)
     #循环访问图片地址保存到本地
     x= 0
     for imgurl in imglist:
